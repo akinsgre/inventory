2718b0b12809ea99af19fe1875195363b9ca14d3
Greg Akins <akinsg@ubuntu.(none)>
2012-05-25 12:24:57 -0700

    Moved jquery images around to fix jquery-ui configuration (Then realized it was just a typo on jquery_ui vs jquery-ui

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,19 +1,25 @@
-  $(function() {
-  $( ".column" ).sortable({
-  connectWith: ".column"
+$(function() {
+      $( ".column" ).sortable({
+				  connectWith: ".column"
+			      });
+      var portlet = $(".portlet");
+      portlet.addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+	  .find( ".portlet-header" )
+	  .addClass( "ui-widget-header ui-corner-all" )
+	  .prepend( "<span class='ui-icon ui-icon-minusthick'></span>")
+
+
+
+
+      $( ".column" ).disableSelection();
   });
 
-  $( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-  .find( ".portlet-header" )
-  .addClass( "ui-widget-header ui-corner-all" )
-  .prepend( "<span class='ui-icon ui-icon-minusthick'></span>")
-  .end()
-  .find( ".portlet-content" );
+$( ".portlet-header .ui-icon" ).click(function() {
 
-  $( ".portlet-header .ui-icon" ).click(function() {
-  $( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
-  $( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
-  });
+					  $( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
+					  $( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
+				      });
+$("#newitem").click(function() {
 
-  $( ".column" ).disableSelection();
-  });
+			$("#column1").append('<div class="portlet"> <div class="portlet-header">Feeds</div> <div class="portlet-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit</div>  </div>').css();
+		    });
