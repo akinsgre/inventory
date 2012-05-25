37b12fade292ffc4aebed3f2096d26d4e77bf8ef
Greg Akins <angrygreg@gmail.com>
2012-05-25 19:03:49 -0400

    Finished updating adding a portlet dynamically with css applying correctly

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -12,14 +12,19 @@ $(function() {
 
 
       $( ".column" ).disableSelection();
-  });
 
-$( ".portlet-header .ui-icon" ).click(function() {
+      $( ".portlet-header .ui-icon" ).click(function() {
 
-					  $( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
-					  $( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
-				      });
-$("#newitem").click(function() {
+						$( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
+						$( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
+					    });
+      $("#newitem").click(function() {
+			      $("#column1").append('<div class="portlet"> <div id="portlet-header-1" class="portlet-header">Feeds</div> <div class="portlet-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit</div>  </div>')
+				  .find('.portlet')
+				  .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+				  .find( "#portlet-header-1" )
+				  .addClass( "ui-widget-header ui-corner-all" )
+				  .prepend( "<span class='ui-icon ui-icon-minusthick'></span>");
+			  });
 
-			$("#column1").append('<div class="portlet"> <div class="portlet-header">Feeds</div> <div class="portlet-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit</div>  </div>').css();
-		    });
+  }) ;
